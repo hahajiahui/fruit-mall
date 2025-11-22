9b848c2255c8af42fd32b5d5a8e3d31b65a5cf72
hahahjiahui <tasijiahui@gmail.com>
2025-11-22 16:24:52 +0800

    初始化 spring boot project

---
+++ fruit-mall/src/test/java/com/jiahui/fruitmall/FruitMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiahui.fruitmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
